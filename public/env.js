08161bf7bf0e3a68488b72de879685827c616f94
Miguel Angel <blacknighthappy@gmail.com>
2025-12-29 16:35:20 -0500

    Remove Cloudflare tunnel config and refactor env.js API base
    Removed '.trycloudflare.com' from allowedHosts in angular.json and refactored public/env.js to use API_ORIGIN for API_BASE instead of a hardcoded Cloudflare tunnel URL. This improves flexibility for different environments and removes Cloudflare-specific configuration.

--- public/env.js
+++ public/env.js
@@ -1,14 +1,9 @@
-(function(window) {
+﻿(function(window) {
   window.__env = window.__env || {};
-  
 
-// API URL Tunnel de cloudflared para poder compartir la aplicacion 
+  const API_ORIGIN = window.__env.API_ORIGIN || 'http://localhost:42420';
 
-  const RENDER_URL = 'https://cow-dale-encouraged-aid.trycloudflare.com'
-
- 
-  window.__env.API_BASE = RENDER_URL + '/api';
-  // Core modules
+  window.__env.API_BASE = API_ORIGIN + '/api';
   window.__env.API_AUTH = window.__env.API_BASE + '/auth';
   window.__env.API_EQUIPOS = window.__env.API_BASE + '/equipos';
   window.__env.API_INSUMOS = window.__env.API_BASE + '/insumos';
@@ -21,10 +16,8 @@
   window.__env.API_SOLICITUDES = window.__env.API_BASE + '/solicitudes';
   window.__env.API_VOLUMETRICOS = window.__env.API_BASE + '/volumetricos';
   window.__env.API_REFERENCIA = window.__env.API_BASE + '/referencia';
-  // Joined endpoints for solicitudes
   window.__env.API_SOLICITUDES_DETALLE = window.__env.API_SOLICITUDES + '/detalle';
   window.__env.API_SOLICITUDES_DETALLE_LISTA = window.__env.API_SOLICITUDES_DETALLE + '/lista';
-  // Equipos submodules
   window.__env.API_HISTORIAL_HV = window.__env.API_EQUIPOS + '/historial';
   window.__env.API_INTERVALO_HV = window.__env.API_EQUIPOS + '/intervalo';
   window.__env.API_FICHA_TECNICA = window.__env.API_EQUIPOS + '/ficha-tecnica';
